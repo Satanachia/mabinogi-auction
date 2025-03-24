62566f46c1aa60c144d746b4ed9194b4d480348d
fdgh134 <fdgh134@naver.com>
2025-03-24 14:45:06 +0900

    vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import MabinogiAuctionPage from "./components/MabiAuctionPage"
+import { Analytics } from '@vercel/analytics/next';
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
     <div className="font-sans max-w-[1440px] mx-auto bg-white shadow-xl">
       <h1 className="max-w-screen-xl mx-auto text-3xl font-bold p-4">마비노기 경매장 검색</h1>
       <MabinogiAuctionPage />
+      <Analytics />
     </div>
   )
 }
