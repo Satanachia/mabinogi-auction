bb6692171f50fb762104191f2bdbd9801e74cc64
fdgh134 <fdgh134@naver.com>
2025-03-24 15:06:21 +0900

    vercel analytics 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import MabinogiAuctionPage from "./components/MabiAuctionPage"
-import { Analytics } from '@vercel/analytics/next';
 
 function App() {
 
@@ -7,7 +6,6 @@ function App() {
     <div className="font-sans max-w-[1440px] mx-auto bg-white shadow-xl">
       <h1 className="max-w-screen-xl mx-auto text-3xl font-bold p-4">마비노기 경매장 검색</h1>
       <MabinogiAuctionPage />
-      <Analytics />
     </div>
   )
 }
